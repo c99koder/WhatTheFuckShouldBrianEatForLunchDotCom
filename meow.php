02d373a74cf61de8a8ed0ec5cd5e2edca83345ac
Sam Steele <sam@last.fm>
2014-11-05 11:04:00 -0800

    meowmode

---
+++ meow.php
@@ -0,0 +1,29 @@
+<html>
+<head>
+<title>What The Fuck Should Brian Eat For Lunch</title>
+</head>
+<body>
+<center>
+<h1>Pouncey should fucking eat:</h1>
+<?php
+$things = array(
+"Some fucking meowmein",
+"Some fucking meowchiladas",
+"Some fucking meowbasa",
+"Some fucking meowrogies",
+"Some fucking meow and cheese",
+"A fucking meowernutter sandwich",
+"A fucking purrito",
+"Some fucking spaghetti and hairballs",
+"A fucking meowburger",
+"A fucking hairball meowinara sub",
+"Some fucking meowshed pawtatoes",
+"Some fucking pawcakes"
+);
+
+echo "<h2>" . $things[rand()%count($things)] . "</h2>";
+?>
+<a href="/?">Pouncey Doesn't Fucking Want That</a>
+</center>
+</body>
+</html>
